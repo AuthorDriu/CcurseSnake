50eb8bb30cf873724f258588b71887191c6e7a2c
Andrew <authordriu@yandex.ru>
2023-05-29 00:09:50 +0300

    Added control and some basic game functions. It's buggy a little bit

--- main.c
+++ main.c
@@ -6,15 +6,19 @@
     НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ
     НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ
     НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ
+    НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ НЕ ИСПОЛЬЗУЙ РЕКУРСИЮ
 */
 
 #include <ncurses.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <unistd.h>
 
 #define SCR_WIDTH       20
 #define SCR_HEIGHT      20
+
+#define UPDATE_DELEY    1
 
 #define WALL_CHAR       '#'
 #define WALL_STR        "#"
@@ -23,6 +27,8 @@
 
 #define APPLE           "O"
 
+
+#define PRINT_HELP 1
 
 /*
     ЗМЕЙКА
@@ -187,6 +193,40 @@ int inSnake(SnakeChain* snake, int x, int y)
     }
 }
 
+// Устанавливает значение coords равные coords головы змейки
+int getHead(SnakeChain* snake, Coords* coords)
+{
+    if (snake->next == NULL)
+    {
+        coords->x = snake->coords.x;
+        coords->y = snake->coords.y;
+    }
+    else
+    {
+        getHead(snake->next, coords);
+    }
+
+    return 0;
+}
+
+// Освобождает память, которая была выделена для змейки
+int snakeFree(SnakeChain* snake)
+{
+    if (snake->next == NULL)
+    {
+        free(snake);
+        
+        return 0;
+    }
+    else
+    {
+        snakeFree(snake->next);
+        free(snake);
+
+        return 0;
+    }
+}
+
 /*
     ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ ЯБЛОКИ
 */
@@ -208,6 +248,22 @@ int throwApple(Apple* apple, SnakeChain* snake)
 
 }
 
+int ate(Apple* apple, SnakeChain* snake)
+{
+    if (snake->next == NULL)
+    {
+        if ((snake->coords.x == apple->coords.x) && (snake->coords.y == apple->coords.y))
+        {
+            return 1;
+        }
+        return 0;
+    }
+    else
+    {
+        return ate(apple, snake->next);
+    }
+}
+
 /*
     ОТРИСОВКА
 */
@@ -215,25 +271,26 @@ int throwApple(Apple* apple, SnakeChain* snake)
 // Вырисовывает игровое поле
 int drawField()
 {
-    for (int y = 0; y < SCR_HEIGHT; ++y)
+    char wall[SCR_WIDTH + 1];       
+    wall[SCR_WIDTH] = '\0';
+    for (int x = 0; x < SCR_WIDTH; ++x)
     {
-        if ((y == 0) || (y == SCR_HEIGHT - 1))
-        {
-            char wall[SCR_WIDTH + 1];
-            wall[SCR_WIDTH] = '\0';
+        wall[x] = WALL_CHAR;
+    }
 
-            for (int x = 0; x < SCR_WIDTH; ++x)
-                wall[x] = WALL_CHAR;
-            move(y, 0);
+    for (int y = 0; y < SCR_HEIGHT + 1; ++y)
+    {
+        if ((y == 0) || (y == SCR_HEIGHT))
+        {
             printw(wall);
         }
         else
         {
-            move(y, 0);
             printw(WALL_STR);
-            move(y, SCR_WIDTH - 1);
+            move(y, SCR_WIDTH);
             printw(WALL_STR);
         }
+        move(y, 0);
     }
     return 0;
 }
@@ -267,6 +324,21 @@ int draw(SnakeChain* snake, Apple* apple)
     drawApple(apple);
     drawSnake(snake);
 
+#ifdef PRINT_HELP
+    move(1, SCR_WIDTH + 5);
+    printw("HELP:");
+    move(2, SCR_WIDTH + 10);
+    printw("[ W ] - up");
+    move(3, SCR_WIDTH + 10);
+    printw("[ D ] - right");
+    move(4, SCR_WIDTH + 10);
+    printw("[ S ] - down");
+    move(5, SCR_WIDTH + 10);
+    printw("[ A ] - left");
+    move(7, SCR_WIDTH + 10);
+    printw("[ E ] - exit");
+#endif
+
     move(SCR_HEIGHT, SCR_WIDTH);
 
     return 0;
@@ -281,24 +353,92 @@ int draw(SnakeChain* snake, Apple* apple)
 */
 
 
+short* TO_PLAY;
+char* kbPressedKey;
+short* movementDirection;
+
+void* inputHandle(void* threadData)
+{
+    nodelay(stdscr, 1);
+    while (*TO_PLAY)
+    {
+        *kbPressedKey = getch();
+
+        if ((*kbPressedKey == 'w') || (*kbPressedKey == 'W'))
+        {
+            *movementDirection = 0;
+        }
+        else if ((*kbPressedKey == 's') || (*kbPressedKey == 'S'))
+        {
+            *movementDirection = 1;
+        }
+        else if ((*kbPressedKey == 'a') || (*kbPressedKey == 'A'))
+        {
+            *movementDirection = 2;
+        }
+        else if ((*kbPressedKey == 'd') || (*kbPressedKey == 'D'))
+        {
+            *movementDirection = 3;
+        }
+        else if ((*kbPressedKey == 'e') || (*kbPressedKey == 'E'))
+        {
+            *TO_PLAY = 0;
+        }
+    }
+    pthread_exit(0);
+}
+
 int main(void)
 {
     srand(time(NULL));
     initscr();
+    noecho();
     
     SnakeChain* snake = createNewChain(SCR_WIDTH / 2, SCR_HEIGHT / 2);
-    snakeGrow(snake, 1);
-    snakeGrow(snake, 2);
-    snakeGrow(snake, 2);
-    snakeGrow(snake, 2);
-
     Apple* apple = malloc(sizeof(Apple));
+
     throwApple(apple, snake);
 
-    draw(snake, apple);
-    refresh();
+    movementDirection = malloc(2);
+    *movementDirection = -1;
+    TO_PLAY = malloc(2);
+    kbPressedKey = malloc(1);
 
-    getch();
+    *TO_PLAY = 1;
+
+    void* thread_data = NULL;
+	pthread_t thread;
+ 
+	pthread_create(&thread, NULL, inputHandle, thread_data);
+
+    while (*TO_PLAY)
+    {
+        if (ate(apple, snake))
+        {
+            snakeGrow(snake, *movementDirection);
+            throwApple(apple, snake);
+        }
+        else
+        {
+            snakeMove(snake, *movementDirection);
+        }
+
+        clear();
+        refresh();
+        usleep(200);
+        draw(snake, apple);
+        usleep(200000);        
+    }
+
     endwin();
+
+    pthread_join(thread, NULL);
+
+    snakeFree(snake);
+    free(movementDirection);
+    free(apple);
+    free(TO_PLAY);
+    free(kbPressedKey);
+
     return 0;
 }
